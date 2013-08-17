8d506bf105358f5690fd40cdfff11f8ff307c24c
RangerMauve <RangerMauve@hotmail.com>
2013-08-16 22:28:02 -0400

    More chat code

--- public/scripts/Chat.js
+++ public/scripts/Chat.js
@@ -1,6 +1,11 @@
 // Make sure you have socket.io and JQuery
 Chat = (function(){
 	function requestConnect(room,name,fail){
-		
+		var chatsocket = null;
+		$.getJSON("/chat/request/"+room,function(data){
+			console.log("Chat Request to "+room+":");
+			console.log(data);
+			socket = io.connect("/chat/+"room);
+		});
 	}
 })();
