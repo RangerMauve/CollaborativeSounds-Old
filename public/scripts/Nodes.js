68c80650bcc90c1e8fbc3367c02c85ec446ad4ed
RangerMauve <RangerMauve@hotmail.com>
2013-08-17 10:23:06 -0400

    Added methods for declaring node types and creating them

--- public/scripts/Nodes.js
+++ public/scripts/Nodes.js
@@ -1,7 +1,44 @@
 Nodes = (function(){
 	var types = {};
+	var list = {};
 	
-	var defaultLocation;
-
-
+	function ranString(length){
+		var s = [0,1,2,3,4,5,6,7,8,8,'A','B','C','D','E','F'];
+		var res = "";
+		for(var i = 0; i < length; i++)
+			res+= s[Math.floor(Math.random()*s.length)]||"";
+		return res;
+	}
+	
+	function register(name, structure, init){
+		if(!init){
+			init = structure;
+			structure = document.getElementById(name).innerHTML;
+		}
+		types[name] = {
+			name:name,
+			structure:structure,
+			init:init
+		}
+	}
+	
+	function create(name){
+		var cont = document.createElement("div");
+		var nid = "Node"+ranString(4);
+		while(!document.getElementById(nid))
+			nid = "Node"+ranString(4);
+		cont.id = nid;
+		cont.innerHTML = types.default.structure;
+		cont.querySelector(".content").innerHTML = types[name].structure;
+		types.default.init(cont);
+		types[name].init(cont);
+	}
+	
+	return {
+		structure:function(name){
+			return types[name];
+		},
+		register:register,
+		create:create
+	}
 })();
