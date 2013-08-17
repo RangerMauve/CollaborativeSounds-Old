6630a0c83e218d669e1cfdf310a88bb12b335fbd
RangerMauve <RangerMauve@hotmail.com>
2013-08-16 20:25:18 -0400

    Added some shit

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ module.exports = function(app,io){
 // Read other files in dir
 	var files = fs.readdirSync(__dirname);
 	for(var i = 0; i < files.length; i++){
-		if(files[i] != "index.js"){
+		if(files[i].indexOf("index") < 0){
+			console.log("Loading "+files[i]);
 			require(require('path').join(__dirname, files[i]))(app,io);
 		}
 	}
