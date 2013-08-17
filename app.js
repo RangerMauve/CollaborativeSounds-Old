a8f5bd1a54a971fc53f51fe0d2fbed36df8ee86a
Ian Frosst <ianjfrosst@gmail.com>
2013-08-17 17:22:45 -0400

    Fixed extra space

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var express = require('express'),
 
 // all environments
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP);
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 80);
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(app.router);
@@ -34,7 +34,7 @@ if(app.get("ip")){
 	});
 } else {
 	server.listen(app.get('port'),app.get('ip'),function(){
-		 console.log('Express server listening on  port:' + app.get('port'));
+		console.log('Express server listening on  port:' + app.get('port'));
 	});
 }
 
