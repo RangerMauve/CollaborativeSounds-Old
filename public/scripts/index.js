d74d14caeac81f1d32b5a000a8bc45c610c228f1
RangerMauve <RangerMauve@hotmail.com>
2013-08-18 06:02:42 -0400

    Removed useless scripts, moved scripts in main page to /scripts/index/js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,15 +1,46 @@
+Nodes.init();
+		function initChat(username){
+			var out = document.getElementById("chatter");
+			function append(message){
+				out.innerHTML+=message;
+			}
+		
+			Chat.connect("test", username, function(err,controls){
+				if(err)append('<div style="color:#F00">Error:'+err+'</div>');
+				
+				$("#chatinput").keyup(function(e){
+					if(e.which == 13)
+						controls.message($("#chatinput").val(),function(err){
+							if(err)append('<div style="color:#F00">Error:'+err+'</div>');
+							else $("#chatinput").val("");
+						});
+				});
+				
+				$("#chatsubmit").click(function(e){
+					console.log("Clicked");
+					controls.message($("#chatInput").val(),function(err){
+						if(err)append('<div style="color:#F00">Error:'+err+'</div>');
+						else $("#chatinput").val("");
+					});
+				});
+				
+				controls.onmessage= function(name,message){
+					append("<div>"+name+': <span class="usermessage">'+message+"</span></div>");
+	                
+				}; 
+			});
+		}
+		function login () {
+			$('#loggin').hide();
+			$('#chatwrap').show().draggable();
+			$('#workspace').show();
+			$('#spawn').show().draggable();;
+			
 
+			var username = document.getElementById("username").value;
 
-
-(function helloDerp (){
- 
-    function hideOut(){
-    var el = document.getElementById("out");
-    
-    el.style.display="none"; 
-}   
-
-
-
-return hideOut();
-})();
+			if (username == "") {
+				username = "User"+Math.floor(Math.random()*1337);
+			}
+			initChat(username);
+		}
